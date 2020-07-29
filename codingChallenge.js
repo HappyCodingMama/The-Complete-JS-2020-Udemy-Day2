c22524e8686f93a88307bf46c573be311a9766c2
HappyCodingMama <happycodingmama@gmail.com>
2020-07-30 00:01:03 +0900

    modified

--- codingChallenge.js
+++ codingChallenge.js
@@ -21,3 +21,17 @@ Hint:write another method for this
 hint:we learned a special technique to do exactly that.
 */
 
+function Question(question, answers, correct) {
+  this.question = question;
+  this.answers = answers;
+  this.correct = correct;
+}
+
+var q1 = new Question('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0);
+
+var q2 = new Question('What is the name of this course\'s teacher?', ['John', 'Michael', 'Jonas'], 2);
+
+var q3 = new Question('What does best describe coding?', ['Boring', 'Hard', 'Fun', 'Tedius'], 2);
+
+var question = [q1, q2, q3];
+
