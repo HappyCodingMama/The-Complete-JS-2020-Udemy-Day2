7b1e840dd141d6706fe52626656cc51ca0256203
HappyCodingMama <happycodingmama@gmail.com>
2020-07-29 23:18:24 +0900

    solving some quizzes

--- README.md
+++ README.md
@@ -67,4 +67,17 @@
 
    - class: btn-hold
      add if(gamePlaying) 
-     when player won the game, add -> gamePlaying = false 
+     when player won the game, add -> gamePlaying = false
+
+* extra
+
+  1. a player looses his entire score when he rolls two 6 in a row. After that, it's the next player's turn. 
+(Hint: Always save the previous dice roll in a separate variable) 
+
+2. Add an input field to the HTML where players can set the winning score,
+so that they can change the predefined score of 100.
+HINT: you can read that value with the .value property in JS. This is a good opportunity to use google to figure this out.
+
+3. Add another dice to the game , so that there are two dices now.
+ The player looses his current score when one of the is a 1.
+HINT: you will need CSS to position the second dice, so take a look at the CSS code for the first one.
---
+++ codingChallenge.js
@@ -0,0 +1,23 @@
+/* Let's build a fun quiz game in the console!
+
+1. Build a function constructor called Question to describe a question.
+A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one(choose an adequate data structure here, array, object, etc.)
+
+2. Create a couple of questions using the constructor
+
+3.Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers
+(each question should have a number) Hint: write a method for the Qeustion object for this task.
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6.Check if the answer is correct and print to the console whether the answer is correct or nor
+Hint:write another method for this
+
+7.Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code
+hint:we learned a special technique to do exactly that.
+*/
+
